4ab66e97eefe6eb9dad1cdb5520d8a3380bceccf
Mikael Koskinen <mikael.koskinen@adafy.com>
2020-04-27 08:38:09 +0300

    The project structure

---
+++ src/HourManagement/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace HourManagement
+{
+    public class MainViewModel : Screen
+    {
+        private string _name;
+    
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                NotifyOfPropertyChange();
+            }
+        }
+
+        public MainViewModel()
+        {
+            Name = "Ei tallennettu";
+        }
+
+        public void Test()
+        {
+            Name = "Muutettu";
+        }
+    }
+}
